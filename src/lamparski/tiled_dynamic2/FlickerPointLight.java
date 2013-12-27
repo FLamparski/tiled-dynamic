f29267d404f60d4e8679046c14e1095646061f09
Filip Wieland <filiplamparski@gmail.com>
2013-12-27 22:19:35 +0000

    Added some more randomness to the blinking light
    Ideas for the future: Add parameters to adjust the randomness,
    and refactor the class to be more presentable

--- src/lamparski/tiled_dynamic2/FlickerPointLight.java
+++ src/lamparski/tiled_dynamic2/FlickerPointLight.java
@@ -12,8 +12,8 @@ import box2dLight.RayHandler;
 public class FlickerPointLight extends PointLight {
     
     private short acc = 0;
-    private short speed = 15;
-    private float magnitude = 0.05f;
+    private short speed = 45;
+    private float magnitude = 0.1f;
     private Random rand;
 
     public FlickerPointLight(RayHandler rayHandler, int rays, Color color,
@@ -33,16 +33,16 @@ public class FlickerPointLight extends PointLight {
     @Override
     protected void update() {
         super.update();
+        float localmagn = magnitude;
         try{
-            acc += (short) rand.nextInt(speed);
+            acc += ((short) rand.nextInt(speed) % 360);
+            localmagn = Math.abs((float) rand.nextGaussian() * magnitude);
+            // localmagn = rand.nextFloat() * magnitude;
         } catch (NullPointerException e){
             // This happens at construction.
             acc += 0;
         }
-        if(acc == 360){
-            acc = 0;
-        }
-        setDistance(getDistance() + (MathUtils.sinDeg(acc) * magnitude));
+        setDistance(getDistance() + (MathUtils.sinDeg(acc) * localmagn));
     }
 
 }
